30cc01b82ad25cb90d8af6ec71d9a85f7a2dec22
vande2025 <vanderlei.duarte@escola.pr.gov.br>
2025-04-02 11:09:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+        const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    
+        botaoDeAcessibilidade.addEventListener('click', function() {
+            botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+            opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+        });
+    });
+
+
+
+
+
+
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -13,3 +36,12 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
 });
+
+
+
+
+
+
+
+
+
